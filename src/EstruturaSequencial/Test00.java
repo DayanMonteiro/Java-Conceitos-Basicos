e42ddf6c011be5af5a6ea0cdd3a8cdcca5c035d4
Dayan Monteiro <dmsmamb@gmail.com>
2021-06-09 17:54:09 -0300

    testes do modulo basico

---
+++ src/EstruturaSequencial/Test00.java
@@ -0,0 +1,13 @@
+package EstruturaSequencial;
+
+/*
+Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos.
+ */
+
+public class Test00 {
+    public static void main(String[] args) {
+
+
+    }
+}
